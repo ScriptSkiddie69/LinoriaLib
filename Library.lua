fc8d8f2393db9285e647b32a65e20603c358649f
ScriptSkiddie69 <110344732+ScriptSkiddie69@users.noreply.github.com>
2024-07-03 14:36:20 +0800

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -1184,9 +1184,9 @@ do
 
                 local Key = KeyPicker.Value;
 
-                if Key == 'MB1' or Key == 'MB2' then
-                    return Key == 'MB1' and InputService:IsMouseButtonPressed(Enum.UserInputType.MouseButton1)
-                        or Key == 'MB2' and InputService:IsMouseButtonPressed(Enum.UserInputType.MouseButton2);
+                if Key == 'MouseButton1' or Key == 'MouseButton2' then
+                    return Key == 'MouseButton1' and InputService:IsMouseButtonPressed(Enum.UserInputType.MouseButton1)
+                        or Key == 'MouseButton2' and InputService:IsMouseButtonPressed(Enum.UserInputType.MouseButton2);
                 else
                     return InputService:IsKeyDown(Enum.KeyCode[KeyPicker.Value]);
                 end;
@@ -1258,9 +1258,9 @@ do
                     if Input.UserInputType == Enum.UserInputType.Keyboard then
                         Key = Input.KeyCode.Name;
                     elseif Input.UserInputType == Enum.UserInputType.MouseButton1 then
-                        Key = 'MB1';
+                        Key = 'MouseButton1';
                     elseif Input.UserInputType == Enum.UserInputType.MouseButton2 then
-                        Key = 'MB2';
+                        Key = 'MouseButton2';
                     end;
 
                     Break = true;
@@ -1286,9 +1286,9 @@ do
                 if KeyPicker.Mode == 'Toggle' then
                     local Key = KeyPicker.Value;
 
-                    if Key == 'MB1' or Key == 'MB2' then
-                        if Key == 'MB1' and Input.UserInputType == Enum.UserInputType.MouseButton1
-                        or Key == 'MB2' and Input.UserInputType == Enum.UserInputType.MouseButton2 then
+                    if Key == 'MouseButton1' or Key == 'MouseButton2' then
+                        if Key == 'MouseButton1' and Input.UserInputType == Enum.UserInputType.MouseButton1
+                        or Key == 'MouseButton2' and Input.UserInputType == Enum.UserInputType.MouseButton2 then
                             KeyPicker.Toggled = not KeyPicker.Toggled
                             KeyPicker:DoClick()
                         end;
